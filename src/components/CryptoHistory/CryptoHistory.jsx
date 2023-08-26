ac75792ad57cfbb84128215b1621ade1349efe62
vika-tsymbaliyk <vika.bronytska@gmail.com>
2023-08-26 13:09:49 +0300

    add task4

--- src/components/CryptoHistory/CryptoHistory.jsx
+++ src/components/CryptoHistory/CryptoHistory.jsx
@@ -1,5 +1,26 @@
+import { formatDate } from 'helpers/formatDate';
 import { BaseTable, THead, Th, Tr, Td } from './CryptoHistory.styled';
 
-export const CryptoHistory = () => {
-  return <div>CryptoHistory</div>;
+export const CryptoHistory = ({items}) => {
+  return <BaseTable>
+  <THead>
+    <tr>
+      <Th>№</Th>
+      <Th>PRICE</Th>
+      <Th>AMOUNT</Th>
+      <Th>DATE</Th>
+    </tr>
+  </THead>
+
+  <tbody>
+    {items && items.map(({id, price, amount, date}, index)=>(
+      <Tr key = {id}>
+      <Td>{index+1}</Td>
+      <Td>{price}</Td>
+      <Td>{amount}</Td>
+      <Td>{formatDate(date)}</Td>
+    </Tr>
+    ))}
+  </tbody>
+</BaseTable>;
 };
